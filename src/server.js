259fedf41789b5290d8fd605350d17f3d802970e
unnhao <unnhao@gmail.com>
2019-09-05 09:41:43 +0800

    add view

--- src/server.js
+++ src/server.js
@@ -5,9 +5,17 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/game/index.html')
 })
-
+app.get('/admin', function (req, res) {
+  res.sendFile(__dirname + '/views/admin/index.html')
+})
+app.get('/test', function (req, res) {
+  res.sendFile(__dirname + '/views/test/index.html')
+})
+app.get('*', function (req, res) {
+  res.status(404).sendFile(__dirname + '/views/404.html')
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
